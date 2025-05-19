3a36a33671fda31de0a5ba7193dedea2b88676c8
Cash-boop <cashdodobara@seattleacademy.org>
2025-05-19 21:54:10 +0000

    efasdf

---
+++ Projects/drakethesnake.py
@@ -0,0 +1,107 @@
+# Section 1 - Helper functions (DON'T CHANGE!!)
+import turtle, math, time, random
+def set_background(image_filename):
+	screen = turtle.Screen()
+	try:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+	except:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+def set_image(sprite, image_filename):
+	image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+	screen = turtle.Screen()
+	screen.register_shape(image_file)
+	sprite.shape(image_file)
+def create_sprite(image_filename, x=0, y=0):
+	sprite = turtle.Turtle()
+	set_image(sprite, image_filename)
+	sprite.penup()
+	sprite.goto(x,y)
+	return sprite
+def get_distance(s1, s2):
+	dx = s1.xcor() - s2.xcor()
+	dy = s1.ycor() - s2.ycor()
+	return math.sqrt(dx*dx + dy*dy)
+window = turtle.Screen()
+window.tracer(0)
+
+# Section 2: Setup
+# TODO - create your player character
+# TODO - set your background
+# TODO - set the starting value for your variable
+
+s1 = create_sprite("drake",0,0)
+set_background("grass")
+score = 0
+
+
+
+# Section 3: Controls
+# TODO - define your controls
+# TODO - pick keys for each control
+
+def move_up():
+	s1.setheading(90)
+	s1.forward(75)
+window.onkeypress(move_up,"Up")
+def move_down():
+	s1.setheading(270)
+	s1.forward(75)
+window.onkeypress(move_down,"Down")
+def move_left():
+	s1.setheading(180)
+	s1.forward(75)
+window.onkeypress(move_left,"Left")
+def move_right():
+	s1.setheading(0)
+	s1.forward(75)
+window.onkeypress(move_right,"Right")
+
+
+
+	
+
+
+
+
+
+
+# Section 4: Game Loop
+window.listen()
+timer = 0
+obstacles=[]
+while True:
+	time.sleep(0.1)
+	timer += 1
+	 
+	if timer % 15 == 0 :
+		y_position = random.randint(-150, 150)
+		x_position = random.randint(-150, 150)
+		s2 = create_sprite("applecore", x_position, y_position)
+		s2.setheading(180)
+		obstacles.append(s2) 
+
+	for s2 in obstacles:
+		if get_distance (s1,s2) <50:
+			score+=1
+			obstacles.remove(s2)
+			s2.hideturtle()
+			
+    
+	s1.clear()
+	s1.write(score,font = ("arial",40,"normal"))
+
+	if len(obstacles) == 5:
+		break
+ 	# TODO - code for automatic actions
+
+
+
+
+
+	window.update()
+
+	# if :
+	# 	break
+	
+
+print("Game Over")
